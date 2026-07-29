76706edc76ce64b4a49c0e296bc0a3f1bce628a5
PJ-Caleon <philipjeremiahcaleon@gmail.com>
2026-07-29 21:01:26 +0800

    Auto update: 2026-07-29 21:01

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -6,6 +6,19 @@ return {
       cssls = {},
       vtsls = { enabled = false },
       biome = { enabled = true, config = { tools = { linter = { enabled = true } } } },
+      pylsp = {
+        settings = {
+          pylsp = {
+            plugins = {
+              pycodestyle = { enabled = false },
+              pyflakes = { enabled = true },
+              pylint = { enabled = false },
+              mccabe = { enabled = false },
+              jedi_completion = { enabled = false },
+            },
+          },
+        },
+      },
     },
   },
 }
